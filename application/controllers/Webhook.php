f4e3637071956b30b8557d819896345a51174416
23Pstars <23Pstars@gmail.com>
2017-02-07 16:27:32 +0700

    update method

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -12,13 +12,18 @@ class Webhook extends CI_Controller {
 
   private $bot;
   private $events;
-  private $signature; 
+  private $signature;
   private $user;
 
   function __construct()
   {
     parent::__construct();
     $this->load->model('tebakkode_m');
+
+    // create bot object
+    $httpClient = new CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+    $this->bot  = new LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+
   }
 
   public function index()
@@ -27,7 +32,7 @@ class Webhook extends CI_Controller {
       echo "Hello Coders!";
       header('HTTP/1.1 400 Only POST method allowed');
       exit;
-    } 
+    }
 
     // get request
     $body = file_get_contents('php://input');
@@ -36,5 +41,45 @@ class Webhook extends CI_Controller {
 
     // log every event requests);
     $this->tebakkode_m->log_events($this->signature, $body);
+
+    foreach ($this->events['events'] as $event){
+      // skip group and room event
+      if(! isset($event['source']['userId'])) continue;
+
+      // get user data from database
+      $this->user = $this->tebakkode_m->getUser($event['source']['userId']);
+
+      // respond event
+      if($event['type'] == 'message'){
+
+        if(method_exists($this, $event['message']['type'].'Message')){
+          $this->{$event['message']['type'].'Message'}($event);
+        }
+      } else {
+
+        if(method_exists($this, $event['type'].'Callback')){
+          $this->{$event['type'].'Callback'}($event);
+        }
+
+      }
+    }
+
   }
+
+  private function followCallback($event) {
+    $res = $this->bot->getProfile($event['source']['userId']);
+    if ($res->isSucceeded()) {
+      $profile = $res->getJSONDecodedBody();
+
+      // send welcome message
+      $message = "Salam kenal, " . $profile['displayName'] . "!\n";
+      $message .= "Silakan kirim pesan \"MULAI\" untuk memulai kuis.";
+      $textMessageBuilder = new TextMessageBuilder($message);
+      $this->bot->pushMessage($event['source']['userId'], $textMessageBuilder);
+
+      // save user data
+      $this->tebakkode_m->saveUser($profile);
+    }
+  }
+
 }
--- application/models/Tebakkode_m.php
+++ application/models/Tebakkode_m.php
@@ -15,4 +15,18 @@ class Tebakkode_m extends CI_Model {
 
     return $this->db->insert_id();
   }
+
+  function getUser($userId) {
+    $data = $this->db->where('user_id', $userId)   ->get('users')->row_array();
+    if(count($data) > 0)return $data;
+    return false;
+  }
+
+  function saveUser($profile) {
+      $this->db->set('user_id', $profile['userId'])
+        ->set('display_name', $profile['displayName'])
+        ->insert('users');
+      return $this->db->insert_id();
+  }
+
 }
